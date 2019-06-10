633a40cc37ade20b180fa9d1146eacf0de9fe1a9
Mahmoud Abd Al-Ghany <Av0peac@hotmail.com>
2019-06-10 02:58:02 +0200

    I/O addition.

--- Matrix.h
+++ Matrix.h
@@ -6,6 +6,7 @@
 #include <iomanip>
 #include <math.h>
 #include <limits>
+
 const double EPS = std::numeric_limits<double>::epsilon();
 
 template<typename T> class Matrix {
@@ -71,7 +72,61 @@ public:
 	    }
 	    return tmp;
 	}
+	
+	friend std::ostream& operator<<(std::ostream &out, const Matrix<T> &mat) {
+		for(int i = 0; i < mat.get_rows(); i++) {
+		    out << "{";
+		    for(int j = 0; j < mat.get_cols(); j++) {
+			if(fabs(mat[i][j] - 0) < EPS)
+			    out << std::fixed << std::setprecision(3) << std::setw(6) << (mat[i][j] = 0);
+			else out << std::fixed << std::setprecision(3) << std::setw(6) << mat[i][j];
+			if(j != mat.get_cols() - 1)
+			    out << ' ';
+		    }
+		    out << "}";
+		    if(i != mat.get_rows() - 1)
+			out << ',' << std::endl;
+		}
+		return out;
+	    }
 
+    // rows separated by semicolon.
+    friend std::istream& operator>>(std::istream &in, Matrix<T> &mat) {
+        // [1 2 3; 4 5 6; 7 8 9;]
+        std::string tmp;
+        std::string raw_form;
+        while (getline(in, tmp) && tmp.find(']') == std::string::npos)
+            raw_form += tmp;
+        raw_form += tmp;
+        raw_form.erase(raw_form.begin());
+        raw_form.erase(raw_form.end() - 1);
+
+        size_t num_rows = std::count_if(raw_form.begin(), raw_form.end(),
+                                 [](int c) {
+                                     return c == ';';
+                                 });
+        std::replace_if(raw_form.begin(), raw_form.end(), [](int c) { return c == ';';}, '\n');
+        std::stringstream ss(raw_form);
+        std::vector<std::vector<T>> tmp_data(num_rows, std::vector<T>());
+        int i = 0;
+        while(getline(ss, tmp)) {
+            std::stringstream row_ss(tmp);
+            T data_member;
+            while(row_ss >> data_member) {
+                tmp_data[i].push_back(data_member);
+            }
+            i++;
+        }
+        size_t r = tmp_data.size();
+        size_t c = tmp_data[0].size();
+        for(auto ro : tmp_data) {
+            if(ro.size() != c) {
+                throw std::runtime_error{"Rows can't have different numbers of elements."};
+            }
+        }
+        mat = Matrix<T>(tmp_data);
+        return in;
+    }
 
     static Matrix<T> eye(int N);
 
