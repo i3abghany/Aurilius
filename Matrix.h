85aa44abd5e1829aaa798fe39d4e195ecb116d87
Mahmoud Abd Al-Ghany <Av0peac@hotmail.com>
2019-06-11 20:21:19 +0200

    Used vectors instead of pointers, Added LU decomp.

--- Matrix.h
+++ Matrix.h
@@ -6,19 +6,21 @@
 #include <iomanip>
 #include <math.h>
 #include <limits>
+#include <algorithm>
+#include <sstream>
 
 const double EPS = std::numeric_limits<double>::epsilon();
 
 template<typename T> class Matrix {
-    T **data;
+    std::vector<std::vector<T>> data;
     int cols, rows;
 public:
     Matrix();
-    Matrix(int, int); // initialized diminsions.
+    Matrix(int, int, T = T{}); // initialized diminsions.
     Matrix(T**, int, int);
     Matrix(std::initializer_list<std::initializer_list<T>>);
-    Matrix(std::vector<std::vector<T>>);
-    Matrix<T>& operator=(const Matrix<T>&);
+    explicit Matrix(std::vector<std::vector<T>>);
+    Matrix<T>& operator=(const Matrix<T> &);
 
     int get_cols() const {return this->cols;}
     int get_rows() const {return this->rows;}
@@ -26,10 +28,13 @@ public:
     void exchange_rows(int, int);
     void exchange_cols(int, int);
 
-    static Matrix<T> matmul(const Matrix<T>&, const Matrix<T>&);
-    static Matrix<T> transpose(const Matrix<T>&);
-	static Matrix<T> pow(const Matrix<T>&, const size_t &);
-    friend std::ostream& operator<<(std::ostream &out, const Matrix<T> & mat) {
+    static Matrix<T> matmul(const Matrix<T> &, const Matrix<T> &);
+    static Matrix<T> transpose(const Matrix<T> &);
+    static Matrix<T> upper(Matrix<T>);
+    static std::pair<Matrix<T>, Matrix<T>> LU(const Matrix &);
+    static bool is_symmetric(const Matrix &);
+
+    friend std::ostream& operator<<(std::ostream &out, const Matrix<T> &mat) {
         for(int i = 0; i < mat.get_rows(); i++) {
             out << "{";
             for(int j = 0; j < mat.get_cols(); j++) {
@@ -46,51 +51,24 @@ public:
         return out;
     }
 
-    friend Matrix<T> operator*(const Matrix<T>& a, const Matrix<T>& b) {
-    	Matrix<T> res = matmul(a, b);
-	return res;
-    }
-
     friend Matrix<T> operator + (const Matrix<T>& a, const Matrix<T>& b) {
-    	if(a.get_rows() != b. get_rows() || a.get_cols() != b.get_cols())
-			throw std::runtime_error{"Matrices of different dimensions cannot be added"};
-		Matrix<T> res {a.get_rows(), a.get_cols()};
-		for(int i = 0; i < res.get_rows(); i++) {
-			for(int j = 0; j < res.get_cols(); j++)
-				res[i][j] = (a[i][j] + b[i][j]);
-		}
-		return res;
+        if(a.get_rows() != b. get_rows() || a.get_cols() != b.get_cols())
+            throw std::runtime_error{"Matrices of different dimensions cannot be added together"};
+        Matrix<T> res {a.get_rows(), a.get_cols()};
+        for(int i = 0; i < res.get_rows(); i++) {
+            for(int j = 0; j < res.get_cols(); j++)
+                res[i][j] = (a[i][j] + b[i][j]);
+        }
+        return res;
     }
 
-	Matrix<T> operator *= (const Matrix<T>&);
-	Matrix<T> operator += (const Matrix<T>&);
-	
-	friend Matrix<T> operator -  (const Matrix<T>& a) {
-	    auto tmp = Matrix<T> {a.data, a.get_rows(), a.get_cols()};
-	    for(int i = 0; i < a.get_rows(); i++) {
-	        for(int j = 0; j < a.get_cols(); j++) {
-		    tmp[i][j] = -1 * tmp[i][j];
-  	        }
-	    }
-	    return tmp;
-	}
-	
-	friend std::ostream& operator<<(std::ostream &out, const Matrix<T> &mat) {
-		for(int i = 0; i < mat.get_rows(); i++) {
-		    out << "{";
-		    for(int j = 0; j < mat.get_cols(); j++) {
-			if(fabs(mat[i][j] - 0) < EPS)
-			    out << std::fixed << std::setprecision(3) << std::setw(6) << (mat[i][j] = 0);
-			else out << std::fixed << std::setprecision(3) << std::setw(6) << mat[i][j];
-			if(j != mat.get_cols() - 1)
-			    out << ' ';
-		    }
-		    out << "}";
-		    if(i != mat.get_rows() - 1)
-			out << ',' << std::endl;
-		}
-		return out;
-	    }
+    friend Matrix<T> operator*(const Matrix<T>& a, const Matrix<T>& b) {
+        Matrix<T> res = matmul(a, b);
+        return res;
+    }
+
+    Matrix<T> operator+=(const Matrix<T>&);
+    Matrix<T> operator*=(const Matrix<T>&);
 
     // rows separated by semicolon.
     friend std::istream& operator>>(std::istream &in, Matrix<T> &mat) {
@@ -103,14 +81,8 @@ public:
         raw_form.erase(raw_form.begin());
         raw_form.erase(raw_form.end() - 1);
 
-        size_t num_rows = std::count_if(raw_form.begin(), raw_form.end(),
-										[](int c) {
-											return c == ';';
-										});
-        std::replace_if(raw_form.begin(), raw_form.end(), 
-										[](int c) {
-											return c == ';';
-										}, '\n');
+        size_t num_rows = std::count_if(raw_form.begin(), raw_form.end(), [](int c) { return c == ';';});
+        std::replace_if(raw_form.begin(), raw_form.end(), [](int c) { return c == ';';}, '\n');
         std::stringstream ss(raw_form);
         std::vector<std::vector<T>> tmp_data(num_rows, std::vector<T>());
         int i = 0;
@@ -135,15 +107,30 @@ public:
 
     static Matrix<T> eye(int N);
 
-    T* operator[](int i) const {
-        return data[i];
+    friend Matrix<T> operator - (const Matrix<T>& a) {
+        auto tmp = Matrix<T> {a.data, a.get_rows(), a.get_cols()};
+        for(int i = 0; i < a.get_rows(); i++) {
+            for(int j = 0; j < a.get_cols(); j++) {
+                tmp[i][j] = -1 * tmp[i][j];
+            }
+        }
+        return tmp;
+    }
+
+
+    std::vector<T>& operator[](int i) {
+        return data.at(i);
+    }
+
+    std::vector<T> operator[](int i) const {
+        return data.at(i);
     }
 
     void gaussian_elemination();
     int zero_rows();
     bool is_inconsistent();
 
-    void upper();
+    Matrix<T> upper();
 
     ~Matrix();
 };
