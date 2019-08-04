7b895cb576e5097e164cb5447857e485729124d8
unknown <mahmoudabdalghany@outlook.com>
2019-08-04 13:49:19 +0200

    Migrated to VS, improved determinants.

---
+++ Matrix-Library/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <chrono>
+#include "Matrix.h"
+
+int main() {
+
+	Matrix<double> A{
+			{1, 1},
+			{1, 2},
+			{1, 3},
+			{1, 4},
+			{1, 5},
+	};
+
+	auto b = Matrix<double>::col_vector({ 1, 2, 2, 2, 2 });
+
+	 //project the b vector into the column space of A C(A)
+	 //and then tuck it to make an augmented matrix.
+	A.tuck_cols(Matrix<double>::project_into_col_space(A, b));
+	A.gaussian_elimination(); // will print out c and d in (y = c + d*x), the best-fit line.
+
+	return 0;
+}
