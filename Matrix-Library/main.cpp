5899a4d7885508296ec1de58dee5d1bfe05086e8
unknown <mahmoudabdalghany@outlook.com>
2019-08-07 23:19:13 +0200

    Added trace, ones, and minor improvements.

--- Matrix-Library/main.cpp
+++ Matrix-Library/main.cpp
@@ -17,8 +17,6 @@ int main() {
 	// //and then tuck it to make an augmented matrix.
 	A.tuck_cols(Matrix<double>::project_into_col_space(A, b));
 	A.gaussian_elimination(); // will print out c and d in (y = c + d*x), the best-fit line.
-	
-	std::cout << 5 * Matrix<double>::eye(4) - Matrix<double>::ones(4, 4);
 
 	return 0;
 }
